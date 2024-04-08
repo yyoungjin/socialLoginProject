d6b550a9e0190f2c71e9a2528630e936e9e58979
노영진 <133024268+yyoungjin@users.noreply.github.com>
2024-04-08 18:56:12 +0900

    4/8 수업내용

--- index.js
+++ index.js
@@ -2,19 +2,14 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import cors from 'cors';
 
+import { ping } from './controller/system.js';
+
 const serverPort = 3000;
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 const router = express.Router();
-
-const ping = (req, res) => {
-    const result = true;
-    res.send({result});
-    console.log(res);
-    console.log(req);
-};
 
 router.route('/ping').get(ping);
 
