02247267ca57d531e1560234513aff5cbe0099a8
yyoungjin <emzmfdudwls@naver.com>
2024-05-13 16:13:15 +0900

    changed

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 export default {
-    'COMPANY':'eos',
+    'COMPANY': 'eos',
     'SERVER_PORT':3000,
     'DB': {
-        'DATABASE': 'socialLoginProject', 
-        'PORT': 3306,
-        'DB_CONNECTION_LIMIT': 10
+      'DATABASE': 'socialLoginProject',
+      'PORT': 3306,
+      'DB_CONNECTION_LIMIT': 10
     }
-}
+  }
